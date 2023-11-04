1ada1e206a3050f97eabe9895b9eb6e5da691274
aaronFortuno <aaron.fortuno@protonmail.com>
2023-11-04 07:47:33 +0100

    compileSdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "aaronfortuno.ioc.k11_wellness"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "aaronfortuno.ioc.k11_wellness"
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
