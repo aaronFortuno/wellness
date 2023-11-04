5cdf794a8871e410506dbd2cec7c566cb71de61a
aaronFortuno <aaron.fortuno@protonmail.com>
2023-11-04 07:44:27 +0100

    ui values

---
+++ app/src/main/java/aaronfortuno/ioc/k11_wellness/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package aaronfortuno.ioc.k11_wellness.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(20.dp),
+    medium = RoundedCornerShape(40.dp)
+)
--- app/src/main/java/aaronfortuno/ioc/k11_wellness/ui/theme/Type.kt
+++ app/src/main/java/aaronfortuno/ioc/k11_wellness/ui/theme/Type.kt
@@ -1,16 +1,41 @@
 package aaronfortuno.ioc.k11_wellness.ui.theme
 
+import aaronfortuno.ioc.k11_wellness.R
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+val Agbalumo = FontFamily(
+    Font(R.font.agbalumo_regular)
+)
+
+val Quicksand = FontFamily(
+    Font(R.font.quicksand_light)
+)
+
+val Dosis = FontFamily(
+    Font(R.font.dosis_light)
+)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Agbalumo,
         fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Quicksand,
+        fontWeight = FontWeight.Light,
+        fontSize = 24.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Dosis,
+        fontWeight = FontWeight.Light,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
