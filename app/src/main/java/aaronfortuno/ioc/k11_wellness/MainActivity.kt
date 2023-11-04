62c4f856e861661d15f97fe251be1e16389cb6d7
aaronFortuno <aaron.fortuno@protonmail.com>
2023-11-04 07:17:34 +0100

    first commit

---
+++ app/src/main/java/aaronfortuno/ioc/k11_wellness/MainActivity.kt
@@ -0,0 +1,46 @@
+package aaronfortuno.ioc.k11_wellness
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import aaronfortuno.ioc.k11_wellness.ui.theme.K11WellnessTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            K11WellnessTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    K11WellnessTheme {
+        Greeting("Android")
+    }
+}
