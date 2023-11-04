539b42289d8fad434f2474a1399fde1e746ebf38
aaronFortuno <aaron.fortuno@protonmail.com>
2023-11-04 07:48:10 +0100

    preview composables and base

--- app/src/main/java/aaronfortuno/ioc/k11_wellness/model/SentenceCard.kt
+++ app/src/main/java/aaronfortuno/ioc/k11_wellness/model/SentenceCard.kt
@@ -1,8 +1,26 @@
 package aaronfortuno.ioc.k11_wellness.model
 
+import aaronfortuno.ioc.k11_wellness.ui.theme.K11WellnessTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun SentencCard() {
+fun SentenceCard() {
 
+}
+
+@Preview
+@Composable
+fun SentenceCardLightPreview() {
+    K11WellnessTheme(darkTheme = true) {
+        SentenceCard()
+    }
+}
+
+@Preview
+@Composable
+fun SentenceCardDarkPreview() {
+    K11WellnessTheme(darkTheme = false) {
+        SentenceCard()
+    }
 }
